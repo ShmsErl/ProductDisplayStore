929911dd43fdaf5b486378dc9cffc62a2f738dd2
ShmsErl <erol.sehmus1@gmail.com>
2023-11-08 10:04:42 +0300

    Product veri tabanı nesnesine yeni bir sorgu eklendi.

--- src/main/java/Tobeto/showRoomStore/dataAccess/concretes/ProductRepository.java
+++ src/main/java/Tobeto/showRoomStore/dataAccess/concretes/ProductRepository.java
@@ -3,5 +3,9 @@ package Tobeto.showRoomStore.dataAccess.concretes;
 import Tobeto.showRoomStore.entities.concretes.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> getByProductName(String productName);
 }
