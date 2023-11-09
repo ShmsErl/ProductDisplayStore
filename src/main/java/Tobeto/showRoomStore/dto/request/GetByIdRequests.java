97cfe27717b8f7d8ae1b1c3de6c27754c9c06aeb
ShmsErl <erol.sehmus1@gmail.com>
2023-11-09 14:33:30 +0300

    GetByIdRequests Sınıfı DTO paketinde oluşturuldu.

---
+++ src/main/java/Tobeto/showRoomStore/dto/request/GetByIdRequests.java
@@ -0,0 +1,15 @@
+package Tobeto.showRoomStore.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetByIdRequests {
+
+    private int id;
+
+
+}
