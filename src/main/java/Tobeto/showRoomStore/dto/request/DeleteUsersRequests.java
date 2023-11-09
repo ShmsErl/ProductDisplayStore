39168ee031c4ee33b3165bb26f130865ca359a58
ShmsErl <erol.sehmus1@gmail.com>
2023-11-09 14:28:12 +0300

    DeleteUsersRequest Sınıfı DTO nesnesi olarak oluşturuldu.

---
+++ src/main/java/Tobeto/showRoomStore/dto/request/DeleteUsersRequests.java
@@ -0,0 +1,13 @@
+package Tobeto.showRoomStore.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeleteUsersRequests {
+    private int id;
+
+}
